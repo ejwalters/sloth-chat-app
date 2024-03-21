43be69994a06856300dadb6dc6b115cd26ba4cd5
Eric Walters <ejwalters24@gmail.com>
2024-03-21 11:18:27 -0400

    added upgrade modal

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components';
 import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Modal from '@material-ui/core/Modal';
 import { auth } from "../firebase";
+import Button from '@material-ui/core/Button';
+
 
 function Header() {
 
     const [user] = useAuthState(auth);
+    const [open, setOpen] = useState(false);
+
+    function handleUpgradeClick() {
+        console.log('Upgrade Subscription Clicked');
+        setOpen(true);
+    }
+
+    function handleClose() {
+        setOpen(false);
+    }
+
+    function handleInquireClick() {
+        // Handle the inquire click here
+    }
+
     return <HeaderContainer>
 
         {/* Header Left */}
@@ -28,21 +46,84 @@ function Header() {
         </HeaderSearch>
         {/* Header Right */}
         <HeaderRight>
-            <HelpOutlineIcon />
+            <UpgradeButton variant="contained" onClick={handleUpgradeClick}>
+                Upgrade Subscription
+            </UpgradeButton>
         </HeaderRight>
+
+        {/* Upgrade Modal */}
+        <StyledModal open={open} onClose={handleClose}>
+            <ModalContent>
+                <Plan>
+                    <h2>Individual Plan</h2>
+                    <p>Feature details...</p>
+                    <Button onClick={handleInquireClick}>Inquire</Button>
+                </Plan>
+                <Plan>
+                    <h2>Business Plan</h2>
+                    <p>Feature details...</p>
+                    <Button onClick={handleInquireClick}>Inquire</Button>
+                </Plan>
+                <Plan>
+                    <h2>Enterprise Plan</h2>
+                    <p>Feature details...</p>
+                    <Button onClick={handleInquireClick}>Inquire</Button>
+                </Plan>
+            </ModalContent>
+        </StyledModal>
 
     </HeaderContainer>
 }
 
 export default Header
 
+const StyledModal = styled(Modal)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Plan = styled.div`
+  flex: 1;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 10px;
+  text-align: center;
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  width: 80%;
+  box-shadow: 0 3px 7px rgba(0, 0, 0, 0.3);
+  outline: none;
+`;
+
+const UpgradeButton = styled(Button)`
+    && {
+        background-color: lightgreen;
+        margin-right: 40px;
+        color: black;
+        font-weight: 600;
+    }
+
+    &:hover {
+        background-color: darkgreen;
+    }
+`;
+
 const HeaderRight = styled.div`
     flex: 0.3;
     display: flex;
     align-items: flex-end;
+    justify-content: flex-end;
+    margin-right: 20px;
 
     > .MuiSvgIcon-root {
-        margin-left: auto;
         margin-right: 20px;
     }
 `;
